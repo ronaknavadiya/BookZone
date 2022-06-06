6f57cfbaff0eabf06551419d0c4e00202358ad61
ronaknavadiya <ronaknavadiya123@gmail.com>
2022-06-06 18:10:29 -0400

    Ronak-commit-router structure created

---
+++ Routes/auth.js
@@ -0,0 +1,7 @@
+const  express = require("express")
+const router = express.Router()
+const {getUser,createtUser} = require("../controller/authenticateController")
+
+router.route("/").get(getUser).post(createtUser);
+
+module.exports = router
--- index.js
+++ index.js
@@ -3,13 +3,18 @@ const app = express()
 const cors = require("cors")
 const bodyParser = require('body-parser');
 const connectDB = require("./Database/connect")
-
+const authRouter = require("./Routes/auth")
 require('dotenv').config()
 const port = process.env.port || 5000;
 
 // Middleware
 app.use(cors())
 app.use(bodyParser.json())   // applied to access body of request object
+app.use(express.json());
+
+// routes
+app.use("/api/V1/user",authRouter);
+
 
 
 app.get("/",(req,res)=>{
