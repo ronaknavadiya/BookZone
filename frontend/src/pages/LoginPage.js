ee33d9022fc06259665b67a31132eb3f476e681d
ronaknavadiya <ronaknavadiya123@gmail.com>
2022-06-25 13:31:16 -0400

    commit ronak routing done

---
+++ frontend/src/pages/LoginPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+
+const LoginPage = () => {
+  return (
+    <div>
+      <button>
+        <Link to="/">HomePage</Link>
+      </button>
+    </div>
+  )
+}
+
+export default LoginPage
