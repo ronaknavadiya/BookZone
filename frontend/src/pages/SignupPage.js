e92c67d675039715e5d41fd8c1032459f194bd64
GabaniYash <gabaniy@uwindsor.ca>
2022-06-27 14:24:34 -0400

    Commit Yash Signup and login page UI created

--- frontend/src/pages/SignupPage.js
+++ frontend/src/pages/SignupPage.js
@@ -1,9 +1,69 @@
-import React from 'react'
+import React from "react";
+import "../css/SignupLogin.css";
+import loginpagebook from "../images/loginpagebook.png";
+import { Link } from "react-router-dom";
 
 const SignupPage = () => {
   return (
-    <div>SignupPage</div>
-  )
-}
+    <div className="loginpage">
+      <div className="centerdiv">
+        <div className="row">
+          <div className="col-md-6 divleft">
+            <p className="head">Get's started.</p>
+            <p className="subhead">
+              Already have an account? <Link to="/login">Log in</Link>
+            </p>
+            <hr />
+            <div className="row align-items-center justify-content-center">
+                <form>
+                <div className="form-group">
+                    <label className="label">User name</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="User name"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label className="label">Email address</label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      placeholder="Enter email"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label className="label">Password</label>
+                    <input
+                      type="password"
+                      className="form-control"
+                      placeholder="Password"
+                    />
+                  </div>
 
-export default SignupPage
+                  <button type="submit" className="btn register mt-4">
+                    Register
+                  </button>
+                </form>
+            </div>
+          </div>
+
+          <div className="col-md-6 d-none d-md-block ">
+            <div className="d-flex align-items-center justify-content-center">
+              <img
+                src={loginpagebook}
+                className="img-fluid"
+                style={{
+                  minHeight: 550,
+                }}
+                alt="book"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignupPage;
