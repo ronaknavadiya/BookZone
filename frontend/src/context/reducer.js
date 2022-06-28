ae795df525a6f856c318cc0cb0bc50133b1260f5
ronaknavadiya <ronaknavadiya123@gmail.com>
2022-06-28 11:49:07 -0400

    commit ronak added signup functionality

--- frontend/src/context/reducer.js
+++ frontend/src/context/reducer.js
@@ -1,9 +1,16 @@
-import { DISPLAY_ALERT } from "./actions";
+import { DISPLAY_ALERT, CONFIGUIRE_USER } from "./actions";
 
 const reducer = (state, action) => {
   switch (action.type) {
     case DISPLAY_ALERT:
       return { ...state, isLoading: true };
+    case CONFIGUIRE_USER:
+      return {
+        ...state,
+        userToken: action.payload.jwtToken,
+        userId: action.payload.user._id,
+        user: action.payload.user,
+      };
 
     default:
       throw new Error(`no such action : ${action.type}`);
