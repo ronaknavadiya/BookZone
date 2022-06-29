f672c27ca8d5feb38946c46f5d5c71c1a39f7d8b
ronaknavadiya <ronaknavadiya123@gmail.com>
2022-06-29 10:53:16 -0400

    import to require changed

--- index.js
+++ index.js
@@ -1,45 +1,41 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
-const bodyParser = require('body-parser');
-const connectDB = require("./Database/connect")
-const authRouter = require("./Routes/auth")
-const utillsRouter = require("./Routes/Utils")
-const morgan =  require("morgan")
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const connectDB = require("./Database/connect");
+const authRouter = require("./Routes/auth");
+const utillsRouter = require("./Routes/Utils");
+const morgan = require("morgan");
+require("dotenv").config();
 const port = process.env.port || 5000;
 
-
 // Middleware
-import authenticateUser from "./middleware/auth.js";
-app.use(cors())
-app.use(bodyParser.json())   // applied to access body of request object
+const authenticateUser = require("./middleware/auth.js");
+app.use(cors());
+app.use(bodyParser.json()); // applied to access body of request object
 app.use(express.json());
 
 // routes
-app.use("/api/V1/user",authRouter);
-app.use("/api/V1/user",utillsRouter)
+app.use("/api/V1/user", authRouter);
+app.use("/api/V1/user", utillsRouter);
 
-if (process.env.NODE_ENV !== 'production') {
-  app.use(morgan('dev'))
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
 }
 
-
-app.get("/",(req,res)=>{
-    res.status(200).send("Hello from Backend")
-})
-
-
+app.get("/", (req, res) => {
+  res.status(200).send("Hello from Backend");
+});
 
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URI)
-            app.listen(port,()=>{
-            console.log(`Listening on port: ${port}`);
-        })
-    } catch (error) {
-        console.log("Error: ",error);
-    }
-}
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Listening on port: ${port}`);
+    });
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+};
 
 start();
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,4 @@
-import jwt from "jsonwebtoken";
+const jwt = require("jsonwebtoken");
 
 const auth = async (req, res, next) => {
   const authHeader = req.headers.authorization;
@@ -16,4 +16,4 @@ const auth = async (req, res, next) => {
   }
 };
 
-export default auth;
+module.exports = auth;
