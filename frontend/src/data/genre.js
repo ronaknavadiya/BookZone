efa9bff74173e8182d8020f92e076ed7b156a4d9
ronaknavadiya <ronaknavadiya123@gmail.com>
2022-07-05 17:21:37 -0400

    commit ronak genre.js data file created

---
+++ frontend/src/data/genre.js
@@ -0,0 +1,52 @@
+import endlessconstellation from "../images/background vector/endless-constellation.svg";
+import dragonscales from "../images/background vector/dragon-scales.svg";
+import quantumgradient from "../images/background vector/quantum-gradient.svg";
+import slantedgradient from "../images/background vector/slanted-gradient.svg";
+import winterysunburst from "../images/background vector/wintery-sunburst.svg";
+
+const genre = [
+  { genre: "Adult", image: endlessconstellation },
+  { genre: "Anthologies", image: dragonscales },
+  { genre: "Art", image: quantumgradient },
+  { genre: "Audiobooks", image: slantedgradient },
+  { genre: "Biographies", image: winterysunburst },
+  { genre: "Body", image: quantumgradient },
+  { genre: "Business", image: winterysunburst },
+  { genre: "Children", image: endlessconstellation },
+  { genre: "Comics", image: slantedgradient },
+  { genre: "Contemporary", image: endlessconstellation },
+  { genre: "Cooking", image: endlessconstellation },
+  { genre: "Crime", image: winterysunburst },
+  { genre: "Engineering", image: endlessconstellation },
+  { genre: "Entertainment", image: endlessconstellation },
+  { genre: "Fantasy", image: endlessconstellation },
+  { genre: "Fiction", image: endlessconstellation },
+  { genre: "Food", image: endlessconstellation },
+  { genre: "General", image: endlessconstellation },
+  { genre: "Health", image: endlessconstellation },
+  { genre: "History", image: endlessconstellation },
+  { genre: "Horror", image: endlessconstellation },
+  { genre: "Investing", image: endlessconstellation },
+  { genre: "Literary", image: endlessconstellation },
+  { genre: "Literature", image: endlessconstellation },
+  { genre: "Manga", image: endlessconstellation },
+  { genre: "Media-help", image: endlessconstellation },
+  { genre: "Memoirs", image: endlessconstellation },
+  { genre: "Mind", image: endlessconstellation },
+  { genre: "Mystery", image: endlessconstellation },
+  { genre: "Nonfiction", image: endlessconstellation },
+  { genre: "Religion", image: endlessconstellation },
+  { genre: "Romance", image: endlessconstellation },
+  { genre: "Science", image: endlessconstellation },
+  { genre: "Self", image: endlessconstellation },
+  { genre: "Spirityality", image: endlessconstellation },
+  { genre: "Sports", image: endlessconstellation },
+  { genre: "Superheroes", image: endlessconstellation },
+  { genre: "Technology", image: endlessconstellation },
+  { genre: "Thrillers", image: endlessconstellation },
+  { genre: "Travel", image: endlessconstellation },
+  { genre: "Women", image: endlessconstellation },
+  { genre: "Young", image: endlessconstellation },
+];
+
+export default genre;
