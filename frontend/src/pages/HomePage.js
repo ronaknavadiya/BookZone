81a642aa9743843e20e83a2b02eab0321adab927
GabaniYash <gabaniy@uwindsor.ca>
2022-07-24 17:17:55 -0400

    Commit Yash HomePage Created

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,7 +1,68 @@
-import React from "react";
+import React, { useEffect, useState, useRef, Fragment } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
+import { useAppContext } from "../context/appContext";
+import "../css/Home.css";
+import BookCard from "./BookCard";
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const { setUserIDandToken, user } = useAppContext();
+  const navigate = useNavigate();
+
+  const search = useRef();
+  const [recmbook, setrecmbook] = useState([]);
+
+  const fetchrecmbook = async () => {
+    try {
+      const res = await axios.get(
+        "https://www.googleapis.com/books/v1/volumes?q=subject:fiction"
+      );
+      setrecmbook(res.data.items);
+    } catch (error) {
+      console.log("Error: ", error);
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      setTimeout(() => {
+        navigate("/");
+      }, 3000);
+    } else {
+      navigate("/login");
+    }
+    fetchrecmbook();
+  }, [user, navigate]);
+
+  return (
+    <>
+      <div className="search">
+        <div className="row align-items-center justify-content-center">
+          <div className="col-4">
+            <div className="input-group">
+              <input type="text" className="form-control" ref={search} />
+              <div className="input-group-append">
+                <button className="btn searchbtn " type="button">
+                  Search
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="books">
+        <div className="row">
+          {recmbook.map((book,index) => {
+            return (
+              <Fragment key={index}>
+                <BookCard  book={book}/>
+              </Fragment>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
