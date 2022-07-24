3cec78b403de051001d453310c5b204d444c9f71
ronaknavadiya <ronaknavadiya123@gmail.com>
2022-07-24 19:34:08 -0400

    commit ronak get specific user api done

--- Routes/Utils.js
+++ Routes/Utils.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const { followUnfollowUser } = require("../controller/authenticateController");
-const { AddFavGenre, LikeBooks } = require("../controller/UtillsConroller");
+const {
+  AddFavGenre,
+  LikeBooks,
+  getSpecificUser,
+} = require("../controller/UtillsConroller");
 
 router.route("/followUnfollow").put(followUnfollowUser);
 router.route("/favGenre").post(AddFavGenre);
 router.route("/likeBook").put(LikeBooks);
+router.route("/friendUser").get(getSpecificUser);
 
 module.exports = router;
