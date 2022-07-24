735f8d5d170f5abf1c5e575bd7de37368abe42bd
ronaknavadiya <ronaknavadiya123@gmail.com>
2022-07-24 19:00:21 -0400

    commit ronak like book api created

--- models/Users.js
+++ models/Users.js
@@ -36,15 +36,19 @@ const UserSchema = new mongoose.Schema(
       type: Array,
       default: [],
     },
+    likedBooks: {
+      type: Array,
+      default: [],
+    },
     profilePicture: {
       type: String,
       default:
         "https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg",
     },
-    genre:{
-      type:Array,
-      default:[]
-    }
+    genre: {
+      type: Array,
+      default: [],
+    },
   },
   { timestamps: true }
 );
