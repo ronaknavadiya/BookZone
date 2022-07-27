406d2d36c5b7f6323f2f5e84b7ed80142399937a
urvishkumar1309 <pansuriu@uwindsor.ca>
2022-07-27 11:25:21 -0400

    urvish comit solve fix - merge fix

--- Routes/Utils.js
+++ Routes/Utils.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const { followUnfollowUser } = require("../controller/authenticateController");
-const { AddFavGenre } = require("../controller/UtillsConroller");
-const {UserController}=require("../controller/UserProfileController");;
-router.route("/followUnfollow").put(followUnfollowUser);
-router.route("/favGenre").post(AddFavGenre);
-router.route("/usersearch").post(UserController);
 const {
   AddFavGenre,
   LikeBooks,
   getSpecificUser,
 } = require("../controller/UtillsConroller");
 
+const {UserController}=require("../controller/UserProfileController");;
+
+
+
+
+router.route("/usersearch").post(UserController);
 router.route("/followUnfollow").put(followUnfollowUser);
 router.route("/favGenre").post(AddFavGenre);
 router.route("/likeBook").put(LikeBooks);
